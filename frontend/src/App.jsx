51fac1f29c49dd4314e15eb855eb2b4a265a8380
Divyansh-J <Divyansh02jain@gmail.com>
2024-08-08 00:19:05 +0530

    login page ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React from "react"
+import {Routes, Route} from "react-router-dom"
+import Home from "./pages/Home.jsx"
+import Login from "./pages/Login.jsx"
+import SignUp from "./pages/SignUp.jsx"
 
 const App = () => {
   return (
-    <div >
-
+    <div className="p-4 h-screen flex items-center justify-center ">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      
     </div>
   );
 };
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className=" w-full p-6 rounded-lg shadow-md bg-gray-200">
+        <h1 className="text-3xl font-semibold text-center text-slate-700">
+          Login
+          <span className="text-blue-700"> ChatBuzz</span>
+        </h1>
+
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Email"
+              className="input input-bordered w-full h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              className="input input-bordered w-full h-10"
+            />
+          </div>
+          <Link
+            to={"/signup"}
+            className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+          >
+            {"Don't"} have an account?{" "}
+            <span className="text-blue-600">Sign Up</span>
+          </Link>
+        </form>
+        <div>
+          <button className="btn btn-block btn-sm mt-2 bg-blue-700 text-white">
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
